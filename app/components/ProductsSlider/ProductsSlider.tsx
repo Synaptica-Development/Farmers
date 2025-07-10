7de769cc780804d4b1097fa30650522ed5f267b6
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2025-07-10 14:39:14 +0400

    feat: added slider

---
+++ app/components/ProductsSlider/ProductsSlider.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { useKeenSlider } from 'keen-slider/react';
+import 'keen-slider/keen-slider.min.css';
+import styles from './ProductsSlider.module.scss';
+import ProductCard from '../ProductCard/ProductCard';
+import { useState } from 'react';
+
+const ProductsSlider = () => {
+  const [loaded, setLoaded] = useState(false);
+
+  const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({
+    slides: {
+      perView: 4,
+      spacing: 32,
+    },
+    mode: 'snap',
+    created() {
+      setLoaded(true);
+    },
+  });
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <h2>ხილი</h2>
+        <span className={styles.seeAll}>ყველას ნახვა</span>
+      </div>
+
+      <div className={styles.sliderWrapper}>
+        <div ref={sliderRef} className={`keen-slider ${styles.slider}`}>
+          {[...Array(10)].map((_, i) => (
+            <div key={i} className={`keen-slider__slide ${styles.slide}`}>
+              <ProductCard
+                image="testproduct"
+                productName="გორის ვაშლი"
+                location="თბილისი"
+                farmerName="გურამის ფერმა"
+                isFavorite={false}
+                price={120.5}
+              />
+            </div>
+          ))}
+        </div>
+
+        {/* {loaded && slider && (
+          <>
+            <button
+              className={`${styles.arrow} ${styles.arrowLeft}`}
+              onClick={() => slider.current?.prev()}
+            />
+            <button
+              className={`${styles.arrow} ${styles.arrowRight}`}
+              onClick={() => slider.current?.next()}
+            />
+          </>
+        )} */}
+      </div>
+    </div>
+  );
+};
+
+export default ProductsSlider;
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@ import Advertisements from "./components/Advertisements/Advertisements";
 import Categories from "./components/Categories/Categories";
 import Header from "./components/Header/Header";
 import ProductCard from "./components/ProductCard/ProductCard";
+import ProductsSlider from "./components/ProductsSlider/ProductsSlider";
 
 export default function Home() {
   return (
     <div>
-      <Header/>
-      <Categories/>
-      <Advertisements/>
-      <ProductCard image={"testproduct"} productName={"გორის ვაშლი"} location={"თბილისი"} farmerName={"გურამის ფერმა"} isFavorite={false} price={120.50}/>
+      <Header />
+      <Categories />
+      <Advertisements />
+      <ProductsSlider />
+      <ProductsSlider />
+      <Advertisements />
+      <ProductsSlider />
     </div>
   );
 }
