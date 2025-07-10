e59504660ada8874e2f1138ef207acde9530c81c
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2025-07-10 13:24:18 +0400

    feat: added product card

---
+++ app/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { useState } from 'react';
+import styles from './ProductCard.module.scss';
+import Image from 'next/image';
+import ReusableButton from '../ReusableButton/ReusableButton';
+
+interface ProductCardProps {
+    image: string;
+    productName: string;
+    location: string;
+    farmerName: string;
+    isFavorite: boolean;
+    price: number;
+}
+
+const ProductCard = (props: ProductCardProps) => {
+    const [favorite, setFavorite] = useState<boolean>(props.isFavorite);
+
+    const toggleFavorite = () => {
+        setFavorite((prev) => !prev);
+    };
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.imageSection}>
+                <Image
+                    src={`/${props.image}.jpg`}
+                    alt='product image'
+                    height={170}
+                    width={250}
+                />
+            </div>
+
+            <div className={styles.details}>
+                <div className={styles.headerSection}>
+                    <h3 className={styles.name}>{props.productName}</h3>
+                    <div className={styles.favoriteIcon} onClick={toggleFavorite}>
+                        <Image
+                            src={favorite ? '/whiteHeart.svg' : '/greenHeart.svg'}
+                            alt={favorite ? 'Favorite' : 'Not Favorite'}
+                            width={26}
+                            height={26}
+                        />
+                    </div>
+                </div>
+
+                <p className={styles.farmerName}>{props.farmerName}</p>
+
+                <div className={styles.location}>
+                    <Image
+                        src={'/testLocation.png'}
+                        alt={'location'}
+                        width={20}
+                        height={20}
+                    />
+                    <p>{props.location}</p>
+                </div>
+                <div className={styles.bottomSection}>
+                    <p className={styles.price}>{props.price}₾</p>
+                    <ReusableButton title={'კალათაში დამატება'} size='normal'/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Advertisements from "./components/Advertisements/Advertisements";
 import Categories from "./components/Categories/Categories";
 import Header from "./components/Header/Header";
+import ProductCard from "./components/ProductCard/ProductCard";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header/>
       <Categories/>
       <Advertisements/>
+      <ProductCard image={"testproduct"} productName={"გორის ვაშლი"} location={"თბილისი"} farmerName={"გურამის ფერმა"} isFavorite={false} price={120.50}/>
     </div>
   );
 }
