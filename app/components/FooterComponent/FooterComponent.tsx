b8ec6291f95b0f807d1da0ad366b253f607f4cc3
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2025-07-10 15:37:26 +0400

    feat: added footer

---
+++ app/components/FooterComponent/FooterComponent.tsx
@@ -0,0 +1,49 @@
+import styles from './FooterComponent.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const FooterComponent = () => {
+
+    return (
+        <div className={styles.background}>
+            <div className={styles.footerWrapper}>
+                <div className={styles.footerContent}>
+                    <div className={styles.footerInfo}>
+                        <Image src="/ecobazarLogo.png" alt="Ecobazar Logo" width={183} height={38} />
+                        <p className={styles.footerText}>
+                            სამოტივაციო პატარა ტექსტი სამოტივაციო პატარა ტექსტი სამოტივაციო პატარა ტექსტი სამოტივაციო პატარა ტექსტი სამოტივაციო პატარა ტექსტი სამოტივაციო წერილი
+                        </p>
+
+                        <div className={styles.footerContact}>
+                            <p>(+559) 555 55 55 55</p>
+                            <span>ან</span>
+                            <p>xatia.wereteli@gmail.com</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.footerLinks}>
+                        <h3 className={styles.footerLinksTitle}>გვერდები</h3>
+
+                        <nav className={styles.footerNav}>
+                            <Link href="/">შესახებ</Link>
+                            <Link href="/">ხშირად დასმული კითხვები</Link>
+                            <Link href="/">კონტაქტი</Link>
+                        </nav>
+
+                        <div className={styles.footerSocials}>
+                            <Image src="/twitter.svg" alt="Twitter" width={40} height={40} />
+                            <Image src="/facebook.svg" alt="Facebook" width={40} height={40} />
+                            <Image src="/instagram.svg" alt="Instagram" width={40} height={40} />
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.footerCopyright}>
+                    <p>Ecobazar eCommerce © 2021. All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FooterComponent;
