08f0572f416b3863defa14a5ad6b87b94a59d787
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2025-07-13 18:35:21 +0400

    feat: connect categories to back

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,17 +2,14 @@ import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: 'http://185.49.165.101:5000/api', 
-  withCredentials: true,                     
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://185.49.165.101:5000',
 });
-api.interceptors.request.use(
-  (config: InternalAxiosRequestConfig) => {
-    const token = Cookies.get('token');  
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },
-  (error: AxiosError) => Promise.reject(error),
-);
+
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  const token = Cookies.get('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
 api.interceptors.response.use(
   (response) => response,
