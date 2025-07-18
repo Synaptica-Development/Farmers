3640d5178c12d1fed6b9eeca1d886c0b2db89d22
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2025-07-18 14:35:53 +0400

    feat: navigation between subcategory and subproducts

--- app/subcategories/[categoryId]/page.tsx
+++ app/subcategories/[categoryId]/page.tsx
@@ -1,23 +1,22 @@
-'use client';
-
 import styles from './page.module.scss';
 import Header from '../../components/Header/Header';
-import { use } from 'react';
 import FooterComponent from '@/app/components/FooterComponent/FooterComponent';
 import SubCategories from '@/app/components/SubCategories/SubCategories';
 import SubProductsSection from '@/app/components/SubProductsSection/SubProductsSection';
 
 interface Props {
-  params: Promise<{ id: string }>; 
+  params: Promise<{ categoryId: string }>; 
 }
 
-export default function Subcategories({ params }: Props) {
-  const { id } = use(params); 
+export default async function subcategories({ params }: Props) {
+
+  const { categoryId } = await params;
+
   return (
     <div className={styles.container}>
       <Header />
-      <SubCategories categoryID={id}/>
-      <SubProductsSection categoryID={id}/>
+      <SubCategories categoryID={categoryId}/>
+      <SubProductsSection categoryID={categoryId}/>
       <div style={{marginTop: '72px'}}><FooterComponent/></div>
     </div>
   );
