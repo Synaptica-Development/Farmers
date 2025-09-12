c4e28517d70121a21a6bf91804859fbff009ad12
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2025-09-12 20:40:02 +0400

    feat: refresh token

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,5 @@
 import BASE_URL from '@/app/config/api';
-import axios, { InternalAxiosRequestConfig } from 'axios';
+import axios, { InternalAxiosRequestConfig, AxiosError } from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
@@ -11,5 +11,21 @@ api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+
+api.interceptors.response.use(
+  response => response,
+  (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      api.get(`/api/Auth/refresh`)
+        .then((res) => {
+          Cookies.set('token', res.data.token, { secure: true, sameSite: 'none' });
+        })
+        .catch(() => {
+          window.location.href = '/signin';
+        });
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
