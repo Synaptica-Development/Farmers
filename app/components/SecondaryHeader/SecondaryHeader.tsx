48967d4185f0a45989a3eba1061d8ade56ed7637
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2025-10-11 13:18:30 +0400

    feat:secondary header text active styles

--- app/components/SecondaryHeader/SecondaryHeader.tsx
+++ app/components/SecondaryHeader/SecondaryHeader.tsx
@@ -1,26 +1,54 @@
 'use client';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import styles from './SecondaryHeader.module.scss';
 
 const SecondaryHeader = () => {
+    const pathname = usePathname();
+
     return (
         <nav className={styles.wrapper}>
             <ul className={styles.inner}>
                 <li>
-                    <Link href="/allproducts">პროდუქტები</Link>
+                    <Link
+                        href="/allproducts"
+                        className={pathname === '/allproducts' ? styles.active : ''}
+                    >
+                        პროდუქტები
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/faq">კითხვები</Link>
+                    <Link
+                        href="/faq"
+                        className={pathname === '/faq' ? styles.active : ''}
+                    >
+                        კითხვები
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/agroschool">აგრო სკოლა</Link>
+                    <Link
+                        href="/agroschool"
+                        className={pathname === '/agroschool' ? styles.active : ''}
+                    >
+                        აგრო სკოლა
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/aboutus">ჩვენს შესახებ</Link>
+                    <Link
+                        href="/aboutus"
+                        className={pathname === '/aboutus' ? styles.active : ''}
+                    >
+                        ჩვენს შესახებ
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/contact">კონტაქტი</Link>
+                    <Link
+                        href="/contact"
+                        className={pathname === '/contact' ? styles.active : ''}
+                    >
+                        კონტაქტი
+                    </Link>
                 </li>
             </ul>
         </nav>
